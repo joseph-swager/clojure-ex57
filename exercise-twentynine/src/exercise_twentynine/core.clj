ffde96a8ae2e84bf2f5b23da044a30d26691548f
joseph-swager <swager@sportsculture.io>
2023-09-14 17:23:54 -0400

    fixed what the linter told me was redundant lets

--- exercise-twentynine/src/exercise_twentynine/core.clj
+++ exercise-twentynine/src/exercise_twentynine/core.clj
@@ -10,10 +10,10 @@
 (defn validate-input [input]
   (let [trimmed-input (-> input
                            (string/trim)
-                           (string/replace #"\s" ""))]
-    (let [parsed-value (try
-                         (Double. trimmed-input)
-                         (catch Exception _ nil))]
+                           (string/replace #"\s" ""))
+        parsed-value (try
+                        (Double. trimmed-input)
+                        (catch Exception _ nil))]
       (cond
         (nil? parsed-value)
           {:value input :valid false :reason "Sorry. That's not a valid input because it's not a number."}
@@ -22,24 +22,22 @@
           {:value parsed-value :valid false :reason "Sorry. That's not a valid input because zero or negative values aren't valid rates of return."}
         
         :else
-          {:value parsed-value :valid true :reason nil}))))
+          {:value parsed-value :valid true :reason nil})))
 
 (defn prompt-for-input []
   (loop []
     (println ask-rate-question)
-    (let [input (string/trim (read-line))]
-      (let [validation-result (validate-input input)]
+    (let [input (string/trim (read-line)) validation-result (validate-input input)]
         (if (:valid validation-result)
           (:value validation-result)
           (do
             (println (:reason validation-result))
-            (recur)))))))
+            (recur))))))
 
 
-(defn execute [& _args]
-  (let [rate-input (prompt-for-input)]
-    (let [years (calculate-years-to-double rate-input)]
-      (println (str "It will take " years " years to double your initial investment.")))))
+(defn- execute [& _args]
+  (let [rate-input (prompt-for-input) years (calculate-years-to-double rate-input)]
+      (println (str "It will take " years " years to double your initial investment."))))
 
 (defn -main [& _args]
   (execute))
